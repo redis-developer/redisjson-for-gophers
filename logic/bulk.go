ed0915e0d1fe87cb9df54e86e287b76d230eb940
Ricardo Ferreira <riferrei@riferrei.com>
2025-01-14 16:42:32 -0500

    Concurrent indexing

--- logic/bulk.go
+++ logic/bulk.go
@@ -12,42 +12,44 @@ import (
 )
 
 func IndexMoviesAsDocuments(ctx context.Context, redisClient *redis.Client, movies []domain.Movie) {
+	const concurrency = 5
 	var waitGroup sync.WaitGroup
+
 	moviesChannel := make(chan struct {
-		id    int
-		movie domain.Movie
+		movieID int
+		movie   domain.Movie
 	}, len(movies))
 
-	for movieID, movie := range movies {
-		waitGroup.Add(1)
-		go func(movieID int, movie domain.Movie) {
-			defer waitGroup.Done()
-			moviesChannel <- struct {
-				id    int
-				movie domain.Movie
-			}{movieID, movie}
-		}(movieID, movie)
-	}
-
 	go func() {
-		waitGroup.Wait()
+		for movieID, movie := range movies {
+			moviesChannel <- struct {
+				movieID int
+				movie   domain.Movie
+			}{movieID, movie}
+		}
 		close(moviesChannel)
 	}()
 
-	pipeline := redisClient.Pipeline()
-	for result := range moviesChannel {
-		movieAsJSON, err := json.Marshal(result.movie)
-		if err != nil {
-			log.Printf("Error marshaling movie into JSON: %v", err)
-			continue
-		}
-		pipeline.JSONSet(ctx, KeyPrefix+strconv.Itoa(result.id+1), "$", string(movieAsJSON))
+	for i := 0; i < concurrency; i++ {
+		waitGroup.Add(1)
+		go func() {
+			defer waitGroup.Done()
+			pipeline := redisClient.Pipeline()
+			for result := range moviesChannel {
+				movieAsJSON, err := json.Marshal(result.movie)
+				if err != nil {
+					log.Printf("Error marshaling movie into JSON: %v", err)
+					continue
+				}
+				pipeline.JSONSet(ctx, KeyPrefix+strconv.Itoa(result.movieID+1), "$", string(movieAsJSON))
+			}
+			_, err := pipeline.Exec(ctx)
+			if err != nil {
+				log.Printf("Error writing JSON documents into Redis: %v", err)
+			}
+		}()
 	}
 
-	_, err := pipeline.Exec(ctx)
-	if err != nil {
-		log.Printf("Error writing JSON documents into Redis: %v", err)
-	}
-
+	waitGroup.Wait()
 	fmt.Printf("🟥 Movies stored on Redis: %d \n", len(movies))
 }
