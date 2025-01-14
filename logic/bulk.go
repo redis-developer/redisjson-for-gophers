c2d6331d494e530447c0a6b301ee91df4feb0807
Ricardo Ferreira <riferrei@riferrei.com>
2025-01-14 16:30:57 -0500

    Concurrent indexing

--- logic/bulk.go
+++ logic/bulk.go
@@ -8,17 +8,41 @@ import (
 	"log"
 	"redisjson4gophers/domain"
 	"strconv"
+	"sync"
 )
 
 func IndexMoviesAsDocuments(ctx context.Context, redisClient *redis.Client, movies []domain.Movie) {
-	pipeline := redisClient.Pipeline()
+	var waitGroup sync.WaitGroup
+	moviesChannel := make(chan struct {
+		id    int
+		movie domain.Movie
+	}, len(movies))
+
 	for movieID, movie := range movies {
-		movie.PlotEmbedding = CreateEmbedding(ctx, movie.Plot)
-		movieAsJSON, err := json.Marshal(movie)
+		waitGroup.Add(1)
+		go func(movieID int, movie domain.Movie) {
+			defer waitGroup.Done()
+			movie.PlotEmbedding = CreateEmbedding(ctx, movie.Plot)
+			moviesChannel <- struct {
+				id    int
+				movie domain.Movie
+			}{movieID, movie}
+		}(movieID, movie)
+	}
+
+	go func() {
+		waitGroup.Wait()
+		close(moviesChannel)
+	}()
+
+	pipeline := redisClient.Pipeline()
+	for result := range moviesChannel {
+		movieAsJSON, err := json.Marshal(result.movie)
 		if err != nil {
 			log.Printf("Error marshaling movie into JSON: %v", err)
+			continue
 		}
-		pipeline.JSONSet(ctx, KeyPrefix+strconv.Itoa(movieID+1), "$", string(movieAsJSON))
+		pipeline.JSONSet(ctx, KeyPrefix+strconv.Itoa(result.id+1), "$", string(movieAsJSON))
 	}
 
 	_, err := pipeline.Exec(ctx)
